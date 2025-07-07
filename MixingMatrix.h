0e52e68868cdd120270b03f6d4472ef427ead9eb
elisaaferraraa <elisa.ferrara@epfl.ch>
2025-07-07 08:58:41 +0200

    Version up to date with current one

--- MixingMatrix.h
+++ MixingMatrix.h
@@ -3,7 +3,7 @@
 #include <Eigen/Dense>
 
 template <typename T>
-constexpr const T& clamp(const T& v, const T& lo, const T& hi) {
+constexpr const T clamp(const T& v, const T& lo, const T& hi) {
     return (v < lo) ? lo : (v > hi) ? hi : v;
 }
 
